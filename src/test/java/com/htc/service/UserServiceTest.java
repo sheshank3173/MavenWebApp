a8e4e8ada4ea94d58ffc50ecb15dec1d48b63f70
Gopi <gopi.officials@gmail.com>
2017-09-21 17:12:26 +0530

    ThirdCommit

---
+++ src/test/java/com/htc/service/UserServiceTest.java
@@ -0,0 +1,31 @@
+package com.htc.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.htc.model.Login;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/Spring-Context.xml")
+public class UserServiceTest {
+
+	@Autowired
+	private UserService userService;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Login login = new Login();
+		login.setUsername("test");
+		
+		userService.validateUser(login);
+	}
+
+}
