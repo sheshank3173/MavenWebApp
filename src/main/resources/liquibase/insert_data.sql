01b04dd2537b7701c22efddd3d07f9a9258e33c7
gopim <gopim@IMPC3122.htcedu.com>
2017-10-10 12:53:59 +0530

    updated insert_data.sql

--- src/main/resources/liquibase/insert_data.sql
+++ src/main/resources/liquibase/insert_data.sql
@@ -3,15 +3,15 @@
 
 INSERT INTO users(
             username, password, firstname, lastname, email, address, phone)
-    VALUES ("Anish";"123Welcome";"Anish";"kumar";"anish!@htcindia.com";"Chennai";123456987);
+    VALUES ("Anish","123Welcome","Anish","kumar","anish!@htcindia.com","Chennai",123456987);
 
 
 INSERT INTO users(
             username, password, firstname, lastname, email, address, phone)
-    VALUES ("Gopi";"password";"Gopi";"Muruganantham";"gopi.officials@gmail.com";"Chennai";123456789);
+    VALUES ("Gopi","password","Gopi","Muruganantham","gopi.officials@gmail.com","Chennai",123456789);
 
 
 INSERT INTO users(
             username, password, firstname, lastname, email, address, phone)
-    VALUES ("sayooj";"123Welcome";"sayooj";"panakal";"sayoojp@htcindia.com";"Chennai";123456);
+    VALUES ("sayooj","123Welcome","sayooj","panakal","sayoojp@htcindia.com","Chennai",123456);
 
